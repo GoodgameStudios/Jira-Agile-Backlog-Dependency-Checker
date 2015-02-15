781a4171f276d40c67067e4d64927cb782a9a3e2
Bengt Brodersen <qoomon@users.noreply.github.com>
2015-02-15 19:20:17 +0100

    Update bookmarkletLoader.js

--- bookmarkletLoader.js
+++ bookmarkletLoader.js
@@ -2,7 +2,7 @@
 javascript:(function(){
         var head = document.getElementsByTagName("head")[0];
         var scriptElement = document.createElement("script");
-        scriptElement.src = <URL to Script>;	
+        scriptElement.src = 'https://qoomon.github.io/Jira-Agile-Backlog-Dependency-Checker/bookmarklet.js';	
         head.appendChild(scriptElement);
         head.removeChild(scriptElement);
 })();
